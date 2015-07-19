98f651894092a39e599d380d3792952dfbb784b8
vinnzy <vinnzy@gmail.com>
2015-07-19 17:58:07 +0530

    Matrix Inversion - 2

--- cachematrix.R
+++ cachematrix.R
@@ -46,19 +46,3 @@ cacheSolve <- function(x, ...) {
   x$setinverse(cachedInverse)
   cachedInverse
 }
-
-test = function(mat){
-  ## @mat: an invertible matrix
-  
-  temp = makeCacheMatrix(mat)
-  
-  start.time = Sys.time()
-  cacheSolve(temp)
-  dur = Sys.time() - start.time
-  print(dur)
-  
-  start.time = Sys.time()
-  cacheSolve(temp)
-  dur = Sys.time() - start.time
-  print(dur)
-}
